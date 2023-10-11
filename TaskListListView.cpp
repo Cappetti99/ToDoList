ff5d04298817b2e1ee52ab091771fb4417d468b7
Cappetti <lorenzo.cappetti@stud.unifi.it>
2023-10-11 11:08:44 +0200

    try

--- TaskListListView.cpp
+++ TaskListListView.cpp
@@ -3,35 +3,23 @@
 //
 
 #include "TaskListListView.h"
-//    void OnOpenButtonClick(wxCommandEvent& event);
 
     TaskListListView::TaskListListView(wxWindow* parent, const wxString& title, const std::vector<TaskList>& taskLists)
         : wxPanel(parent, wxID_ANY, wxDefaultPosition, wxDefaultSize), taskLists(taskLists) {
-
-    auto* mainSizer = new wxBoxSizer(wxVERTICAL);
 
     auto* addListButton = new wxButton(this, wxID_ANY, "Add List");
     auto* removeListButton = new wxButton(this, wxID_ANY, "Remove List");
     auto* searchListButton = new wxButton(this, wxID_ANY, "Search List");
     auto* renameListButton = new wxButton(this, wxID_ANY, "Rename List");
 
-    auto* buttonsSizer = new wxBoxSizer(wxHORIZONTAL );
+    auto* buttonsSizer = new wxBoxSizer(wxHORIZONTAL);
 
-    buttonsSizer->Add(addListButton, 5, wxALL, 10);
-    buttonsSizer->Add(removeListButton, 5, wxALL, 10);
-    buttonsSizer->Add(searchListButton, 5, wxALL, 10);
-    buttonsSizer->Add(renameListButton, 5, wxALL, 10);
+    buttonsSizer->Add(addListButton, 0, wxALL, 10);
+    buttonsSizer->Add(removeListButton, 0, wxALL, 10);
+    buttonsSizer->Add(searchListButton, 0, wxALL, 10);
+    buttonsSizer->Add(renameListButton, 0, wxALL, 10);
 
-    mainSizer->Add(buttonsSizer, 0, wxALIGN_LEFT);
+    this->SetSizer(buttonsSizer);
 
-    wxBoxSizer* listBoxSizer = new wxBoxSizer(wxVERTICAL);
-
-    taskListList = new wxListBox(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0, nullptr, wxLB_SINGLE);
-    listBoxSizer->Add(taskListList, 1, wxEXPAND | wxALL, 10);
-
-    mainSizer->Add(listBoxSizer, 1, wxEXPAND | wxALL, 5);
-
-    this->SetSizer(mainSizer);
 }
 
-//sistemato finestra e aggiunto listbox
